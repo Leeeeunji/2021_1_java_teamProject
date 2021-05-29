6284cc6ac132fe2a5b625575583a6ca465503762
Leeeeunji <72423836+Leeeeunji@users.noreply.github.com>
2021-05-30 02:07:45 +0900

    Update fightScreen.java

--- fightScreen.java
+++ fightScreen.java
@@ -3,19 +3,29 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
 
+//HARDëª¨ë“œ Play Screen
 
-public class fightScreen extends JFrame implements ActionListener{
+public class fightScreen extends JFrame implements ActionListener, Starter {
 
-	public static final int WINDOW_WIDTH = 1280;
-	public static final int WINDOW_HEIGHT = 720;
-	
+// ìŠ¤í‚¬ì´í™íŠ¸, í¬ì…˜ì´í™íŠ¸
+	ImageIcon sk1, sk2, sk3, sk4, po;
+
+// í”Œë ˆì´ì–´ ë° ëª¬ìŠ¤í„° ê³µê²©ë°›ì„ë•Œ, ì•„ë¬´ê²ƒë„ì•ˆí• ë•Œ, ê³µê²©í• ë•Œ
+	ImageIcon p_attacked, p, p_attacking;
+	ImageIcon m_attacked, m, m_attacking;
+
+	public static void main(String[] args) {
+		fightScreen s = new fightScreen();
+		s.setVisible(true);
+	}
+
 	public fightScreen() {
-		setTitle("fight!");
-		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-		
+
+		setting();
+
 		ImageIcon back;
-		back = new ImageIcon("C:\\Users\\ÀÌÀºÁö\\eclipse-workspace\\FinalTeamProject\\src\\fight.png");
-		
+		back = new ImageIcon("C:\\Users\\ì´ì€ì§€\\eclipse-workspace\\FinalTeamProject\\src\\fight.png");
+
 		JPanel screen = new JPanel() {
 			public void paintComponent(Graphics g) {
 				g.drawImage(back.getImage(), 0, 0, null);
@@ -25,86 +35,112 @@ public class fightScreen extends JFrame implements ActionListener{
 		};
 		screen.setLayout(null);
 
-//½ºÅ³¹öÆ°
+
+		sk1 = new ImageIcon("C:\\Users\\ì´ì€ì§€\\eclipse-workspace\\FinalTeamProject\\src\\prac.png");
+		sk2 = new ImageIcon("C:\\Users\\ì´ì€ì§€\\eclipse-workspace\\FinalTeamProject\\src\\prac.png");
+		sk3 = new ImageIcon("C:\\Users\\ì´ì€ì§€\\eclipse-workspace\\FinalTeamProject\\src\\prac.png");
+		sk4 = new ImageIcon("C:\\Users\\ì´ì€ì§€\\eclipse-workspace\\FinalTeamProject\\src\\prac.png");
+		po = new ImageIcon("C:\\Users\\ì´ì€ì§€\\eclipse-workspace\\FinalTeamProject\\src\\prac.png");
+		p_attacked = new ImageIcon("C:\\Users\\ì´ì€ì§€\\eclipse-workspace\\FinalTeamProject\\src\\prac.png");
+		p = new ImageIcon("C:\\Users\\ì´ì€ì§€\\eclipse-workspace\\FinalTeamProject\\src\\prac.png");
+		p_attacking = new ImageIcon("C:\\Users\\ì´ì€ì§€\\eclipse-workspace\\FinalTeamProject\\src\\prac.png");
+		m_attacked = new ImageIcon("C:\\Users\\ì´ì€ì§€\\eclipse-workspace\\FinalTeamProject\\src\\prac.png");
+		m = new ImageIcon("C:\\Users\\ì´ì€ì§€\\eclipse-workspace\\FinalTeamProject\\src\\prac.png");
+		m_attacking = new ImageIcon("C:\\Users\\ì´ì€ì§€\\eclipse-workspace\\FinalTeamProject\\src\\prac.png");
+		
+//ìŠ¤í‚¬ë²„íŠ¼
 		JPanel BtnPanel = new JPanel();
 		BtnPanel.setLayout(new GridLayout(1, 4));
-		
+
 		JButton skill1 = new JButton();
 		skill1.addActionListener(this);
 		skill1.setActionCommand("skill1");
-		//skill1.setBorderPainted(false);
-		//skill1.setContentAreaFilled(false);
-		//skill1.setFocusPainted(false);
-		ImageIcon skill1Icon = new ImageIcon("C:\\Users\\ÀÌÀºÁö\\eclipse-workspace\\FinalTeamProject\\src\\prac.png");
-		skill1.setIcon(skill1Icon);
-		
+		// skill1.setBorderPainted(false);
+		// skill1.setContentAreaFilled(false);
+		// skill1.setFocusPainted(false);
+		skill1.setIcon(sk1);
+
 		JButton skill2 = new JButton();
 		skill2.addActionListener(this);
 		skill2.setActionCommand("skill2");
-		//skill2.setBorderPainted(false);
-		//skill2.setContentAreaFilled(false);
-		//skill2.setFocusPainted(false);
-		ImageIcon skill2Icon = new ImageIcon("C:\\Users\\ÀÌÀºÁö\\eclipse-workspace\\FinalTeamProject\\src\\prac.png");
-		skill2.setIcon(skill2Icon);
-		
+		// skill2.setBorderPainted(false);
+		// skill2.setContentAreaFilled(false);
+		// skill2.setFocusPainted(false);
+		skill2.setIcon(sk2);
+
 		JButton skill3 = new JButton();
 		skill3.addActionListener(this);
 		skill3.setActionCommand("skill3");
-		//skill3.setBorderPainted(false);
-		//skill3.setContentAreaFilled(false);
-		//skill3.setFocusPainted(false);
-		ImageIcon skill3Icon = new ImageIcon("C:\\Users\\ÀÌÀºÁö\\eclipse-workspace\\FinalTeamProject\\src\\prac.png");
-		skill3.setIcon(skill3Icon);
-		
+		// skill3.setBorderPainted(false);
+		// skill3.setContentAreaFilled(false);
+		// skill3.setFocusPainted(false);
+		skill3.setIcon(sk3);
+
 		JButton skill4 = new JButton();
 		skill4.addActionListener(this);
 		skill4.setActionCommand("skill4");
-		//skill4.setBorderPainted(false);
-		//skill4.setContentAreaFilled(false);
-		//skill4.setFocusPainted(false);
-		ImageIcon skill4Icon = new ImageIcon("C:\\Users\\ÀÌÀºÁö\\eclipse-workspace\\FinalTeamProject\\src\\prac.png");
-		skill4.setIcon(skill4Icon);
-		
+		// skill4.setBorderPainted(false);
+		// skill4.setContentAreaFilled(false);
+		// skill4.setFocusPainted(false);
+		skill4.setIcon(sk4);
+
 		BtnPanel.add(skill1);
 		BtnPanel.add(skill2);
 		BtnPanel.add(skill3);
 		BtnPanel.add(skill4);
-		
+
 		BtnPanel.setBounds(800, 520, 400, 100);
 		screen.add(BtnPanel);
 
-//Æ÷¼Ç ¹öÆ°		
+//í¬ì…˜ ë²„íŠ¼		
 		JButton potion = new JButton();
 		potion.addActionListener(this);
 		potion.setActionCommand("potion");
-		//potion.setBorderPainted(false);
-		//potion.setContentAreaFilled(false);
-		//potion.setFocusPainted(false);
+		// potion.setBorderPainted(false);
+		// potion.setContentAreaFilled(false);
+		// potion.setFocusPainted(false);
 		potion.setBounds(1070, 380, 125, 125);
-		ImageIcon potionIcon = new ImageIcon("C:\\Users\\ÀÌÀºÁö\\eclipse-workspace\\FinalTeamProject\\src\\prac.png");
-		potion.setIcon(potionIcon);
+		potion.setIcon(po);
 		screen.add(potion);
-		
-//ÁÖÀÎ°ø
-		JLabel hero = new JLabel();
-		ImageIcon heroIcon = new ImageIcon("C:\\Users\\ÀÌÀºÁö\\eclipse-workspace\\FinalTeamProject\\src\\prac.png");
-		hero.setIcon(skill1Icon);
-		screen.add(hero);
-		
-//¸ó½ºÅÍ		
-		
+
+//ì£¼ì¸ê³µ
+		JLabel player = new JLabel();
+		player.setIcon(p); //ê¸°ë³¸ê°’
+		player.setBounds(50, 420, 200, 200);
+		screen.add(player);
+//ëª¬ìŠ¤í„°		
+		JLabel monster = new JLabel();
+		monster.setIcon(m); //ê¸°ë³¸ê°’
+		monster.setBounds(500, 200, 200, 200);
+		screen.add(monster);
+
 		add(screen);
-	}
-	
-	public static void main(String[] args) {
-		fightScreen s = new fightScreen();
-		s.setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+		String ac = e.getActionCommand();
+		if (ac.equals("skill1")) {
+
+		} else if (ac.equals("skill2")) {
+
+		} else if (ac.equals("skill3")) {
+
+		} else if (ac.equals("skill4")) {
+
+		} else if (ac.equals("potion")) {
+
+		} else {
+			System.err.println("ë²„íŠ¼ ì„ íƒ ì—ëŸ¬");
+		}
+	}
+
+	@Override
+	public void setting() {
+		// TODO Auto-generated method stub
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		setTitle("<HARD> ì‹¸ì›Œë¼. ì´ê²¨ë¼");
 	}
 
 }
