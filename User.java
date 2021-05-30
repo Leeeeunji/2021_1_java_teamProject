e80baa3b510193812fdb2bdba65d73ff4c0f53fd
Leeeeunji <72423836+Leeeeunji@users.noreply.github.com>
2021-05-30 11:51:30 +0900

    파일입출력 위한 serializable 클래스

--- User.java
+++ User.java
@@ -1,4 +1,17 @@
-
-public class User {
-
+import java.io.Serializable;
+public class User implements Serializable {
+	private String id; //아이디
+	private int score;	//학점
+	public User() {
+		score = 0;
+		id = "";
+	}
+	public User(int theNumber, String theLetter) {
+		score = theNumber;
+		id = theLetter;
+	}
+	public String toString() {
+		return "id : " + score
+				+ " score : " + id;
+	}
 }
